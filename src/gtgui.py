e0de7e14cb414d8a5ba0659bc8d115828022891b
nrmalone <nicholasrmalone99@gmail.com>
2022-05-15 16:35:50 -0700

    added duration and volume sliders

--- src/gtgui.py
+++ src/gtgui.py
@@ -1,28 +1,59 @@
+from ctypes import alignment
 import tkinter as tk
 import gttunings
 import gtsound
 
 window = tk.Tk()
 window.title("Simple Guitar Tuner")
-window.geometry("640x480")
 window.resizable(False, False)
-window.configure(bg='#898989')
+window.configure(bg='#1d1d1d')
 window.iconbitmap('./assets/icon.ico')
 
 def tuneClick():
-    gtsound.playTuning(tuningSelection.get(), speed=1000) #TODO: Implement speed slider
+    gtsound.playTuning(tuningSelection.get(), duration=durationSlider.get(), volume=volumeSlider.get()) #TODO: Implement duration/speed & volume sliders
 
+#header
+welcomeLabel = tk.Label(window, text="Welcome to Simple Guitar Tuner!", font=("Arial", 16))
+welcomeLabel.configure(bg='#1d1d1d', fg='#dbdbdb')
+welcomeLabel.grid(row=0,column=0,columnspan=3)
+instructionLabel = tk.Label(window,
+                            text="Instructions:\n1. Select tuning, duration, & volume.\n2. Press Tune button to hear that tuning.\n*Wait for the tuner to finish before making another selection",
+                            font=("Arial", 10))
+instructionLabel.configure(bg='#1d1d1d', fg='#dbdbdb')
+instructionLabel.grid(row=1,column=0,columnspan=3,ipady=5)
+
+#left column: tuning selection
 tuningSelectionLabel = tk.Label(window, text="Select Tuning:")
-tuningSelectionLabel.grid(row=0,column=0)
-
-tuningRow = 1
+tuningSelectionLabel.configure(bg='#1d1d1d', fg='#dbdbdb')
+tuningSelectionLabel.grid(row=3,column=0)
 tuningSelection = tk.StringVar(window, "E Standard")
+tuningRow = 4
 for (text, value) in gttunings.possibleTunings.items():
     tk.Radiobutton(window, text = text, variable = tuningSelection,
                 value = value, indicator = 0,
-                background = "#afafaf").grid(row=tuningRow, column=0)
+                background = "#a5a5a5", foreground='#000000').grid(row=tuningRow, column=0,sticky="ew")
     tuningRow += 1
 
-button = tk.Button(window, text = "Tune", command = tuneClick).grid(row=0,column=2)
+#middle column: duration slider
+durationSliderLabel = tk.Label(window, text="Duration (ms):")
+durationSliderLabel.configure(bg='#1d1d1d', fg='#dbdbdb')
+durationSliderLabel.grid(row=4,column=1,sticky="EW")
+durationSlider = tk.Scale(window, from_=2500, to=250)
+durationSlider.configure(sliderrelief='raised', highlightbackground='#1d1d1d', bg='#1d1d1d', fg='#dbdbdb', cursor='double_arrow', resolution=10)
+durationSlider.set(1000)
+durationSlider.grid(row=5,column=1,rowspan=tuningRow,sticky="NS")
+
+#right column: tune button & volume slider
+tuneButton = tk.Button(window, text = "Tune", font=("Arial", 14), command = tuneClick)
+tuneButton.configure(bg='#a5a5a5', fg='#000000', cursor='exchange')
+tuneButton.grid(row=2,column=2,rowspan=2)
+
+volumeSliderLabel = tk.Label(window, text="Volume:")
+volumeSliderLabel.configure(bg='#1d1d1d', fg='#dbdbdb')
+volumeSliderLabel.grid(row=4,column=2,sticky="EW")
+volumeSlider = tk.Scale(window, from_=100, to=0)
+volumeSlider.configure(sliderrelief='raised', highlightbackground='#1d1d1d', bg='#1d1d1d', fg='#dbdbdb', cursor='double_arrow', resolution=1)
+volumeSlider.set(100)
+volumeSlider.grid(row=5,column=2,rowspan=tuningRow,sticky="NS")
 
 window.mainloop()
--- src/gtsound.py
+++ src/gtsound.py
@@ -1,13 +1,15 @@
 from gtmath import *
 import winsound
+#from subprocess import call
 
-def playTuning(tuning, speed):
+def playTuning(tuning, duration, volume):
+    #call(["amixer", "-D", "pulse", "sset", "Master", str(volume)+"%"])
     if "Standard" in tuning:
         strings = standardCalc(tuning[:2].strip())
         for string in strings:
-            winsound.Beep(string, speed)
+            winsound.Beep(string, duration)
     
     if "Drop" in tuning:
         strings = dropCalc(tuning[-2:].strip())
         for string in strings:
-            winsound.Beep(string, speed)
+            winsound.Beep(string, duration)
